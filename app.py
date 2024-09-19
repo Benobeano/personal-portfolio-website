6009fe3caa132ef20154d6203036de5338b4e1e2
ben Pena <ben647@outlook.com>
2024-09-19 11:53:44 -0500

    change app.py file

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
-import base64
-from flask import Flask
-from app.routes import initialize_routes  # Import your route initialization function if applicable
+# import base64
+# from flask import Flask
+# from app.routes import initialize_routes  # Import your route initialization function if applicable
 
-def create_app():
-    app = Flask(__name__)
+# def create_app():
+#     app = Flask(__name__)
 
-    # Initialize your routes
-    initialize_routes(app)
+#     # Initialize your routes
+#     initialize_routes(app)
 
-    # Define and register the custom filter
-    def b64encode(data):
-        """Encode binary data to a base64 string."""
-        return base64.b64encode(data).decode('utf-8')
+#     # Define and register the custom filter
+#     def b64encode(data):
+#         """Encode binary data to a base64 string."""
+#         return base64.b64encode(data).decode('utf-8')
 
-    # Register the filter with Jinja2
-    app.jinja_env.filters['b64encode'] = b64encode
+#     # Register the filter with Jinja2
+#     app.jinja_env.filters['b64encode'] = b64encode
 
-    return app
+#     return app
+
+
+from app import create_app
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
